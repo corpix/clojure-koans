65556e9c4d7cd67dbeb7403bc102d8c0366d96c0
Dmitry Moskowski <me@corpix.ru>
2019-12-07 16:59:10 +0000

    koans: 15_destructuring

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -1,5 +1,6 @@
 (ns koans.15-destructuring
-  (:require [koan-engine.core :refer :all]))
+  (:require [koan-engine.core :refer :all])
+  (:require [clojure.string :as string]))
 
 (def test-address
   {:street-address "123 Test Lane"
@@ -8,37 +9,49 @@
 
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
-  (= __ ((fn [[a b]] (str b a))
-         [:foo :bar]))
+  (= ":bar:foo" ((fn [[a b]] (str b a))
+                 [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "An Oxford comma list of apples, "
           "oranges, "
           "and pears.")
-     ((fn [[a b c]] __)
+     ((fn [[a b c]] (str "An Oxford comma list of "
+                         a ", " b ", and " c "."))
       ["apples" "oranges" "pears"]))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-       __))
+       (string/join " aka " (conj aliases (str first-name " " last-name)))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+       {:original-parts full-name
+        :named-parts {:first first-name :last last-name}}))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
-     (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+     (let [{street-address :street-address city :city state :state} test-address]
+       (string/join ", " [street-address city state])))
+
+  "Break up maps by key and getting original map"
+  (= test-address
+     (let [{} :as all test-address]
+       all))
+
+  "Break up maps by key with non existent keys"
+  (= nil (let [{does-not-exists :god} test-address] does-not-exists))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [street-address city state]} test-address]
+       (string/join ", " [street-address city state])))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     (let [[[name surname] {:keys [street-address city state]}]
+           [["Test" "Testerson"] test-address]]
+       (string/join ", " [(str name " " surname) street-address city state]))))
