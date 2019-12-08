ed9d561580cbdeed37d62a79a98919021a0ed1af
Dmitry Moskowski <me@corpix.ru>
2019-12-08 23:34:38 +0000

    koans: 19_datatypes

--- src/koans/19_datatypes.clj
+++ src/koans/19_datatypes.clj
@@ -1,47 +1,54 @@
 (ns koans.19-datatypes
   (:require [koan-engine.core :refer :all]))
 
-(defrecord Nobel [prize])
-(deftype Pulitzer [prize])
+(defrecord nobel [prize]) ;; this capitalized names in lispy code looks like awful decision, an eyesore, a dirt on the screen
+(deftype pulitzer [prize])
 
-(defprotocol Award
+(defprotocol award
   (present [this recipient]))
 
-(defrecord Oscar [category]
-  Award
+(defrecord oscar [category]
+  award
   (present [this recipient]
-    (print (str "Congratulations on your "
-                (:category this) " Oscar, "
-                recipient
-                "!"))))
+    (print (str
+            "Congratulations on your "
+            (:category this)
+            " Oscar, " recipient "!"))))
 
-(deftype Razzie [category]
-  Award
+(deftype razzie [category]
+  award
   (present [this recipient]
-    __))
+    (print (str
+            "You're really the "
+            (.category this)
+            ", " recipient "... sorry."))))
 
 (meditations
+ ;;;; fuckin beautiful, antipattern in the koans https://stuartsierra.com/2015/05/17/clojure-record-constructors
+ ;; "Holding records is meaningful only when the record is worthy of you"
+ ;; (= __ (.prize (nobel. "peace")))
+
   "Holding records is meaningful only when the record is worthy of you"
-  (= __ (.prize (Nobel. "peace")))
+  (= "peace" (-> "peace" ->nobel .prize)) ;; same as (.prize (->nobel "peace"))
 
   "Types are quite similar"
-  (= __ (.prize (Pulitzer. "literature")))
+  (= "literature" (-> "literature" ->pulitzer .prize))
 
   "Records may be treated like maps"
-  (= __ (:prize (Nobel. "physics")))
+  (= "physics" (-> "physics" ->nobel :prize))
 
-  "While types may not"
-  (= __ (:prize (Pulitzer. "poetry")))
+  "While types may not" ;; beacuse it does not implement dome «map related» interface
+  (= nil (-> "poetry" ->pulitzer :prize))
 
   "Further study reveals why"
-  (= __
-     (map map? [(Nobel. "chemistry")
-                (Pulitzer. "music")]))
+  (= [true false]
+     (map map? [(->nobel    "chemistry")
+                (->pulitzer "music")]))
 
   "Either sort of datatype can define methods in a protocol"
-  (= __
-     (with-out-str (present (Oscar. "Best Picture") "Evil Alien Conquerors")))
+  (= "Congratulations on your Best Picture Oscar, Evil Alien Conquerors!"
+     (with-out-str (present (->oscar "Best Picture") "Evil Alien Conquerors")))
 
   "Surely we can implement our own by now"
   (= "You're really the Worst Picture, Final Destination 5... sorry."
-     (with-out-str (present (Razzie. "Worst Picture") "Final Destination 5"))))
+     (with-out-str (present (->razzie "Worst Picture") "Final Destination 5"))))
