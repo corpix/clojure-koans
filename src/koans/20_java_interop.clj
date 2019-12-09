0b3202af511be2b16d48d542a2dd4407cd48c4ef
Dmitry Moskowski <me@corpix.ru>
2019-12-09 00:02:34 +0000

    koans: 20_java_interop

--- src/koans/20_java_interop.clj
+++ src/koans/20_java_interop.clj
@@ -1,19 +1,24 @@
 (ns koans.20-java-interop
   (:require [koan-engine.core :refer :all]))
 
+(defn to-upper-case [str] (.toUpperCase str))
+(defn get-count     [v]   (.getCount v))
+(defn pow           [b e] (Math/pow b e))
+(defn make-latch    [n]   (java.util.concurrent.CountDownLatch. n))
+
 (meditations
   "You may have done more with Java than you know"
-  (= __ (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
+  (= java.lang.String (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
+  ;; (javadoc "warfare") => true, WAT? did you mean «type (class "warfare") in the REPL»?
 
   "The dot signifies easy and direct Java interoperation"
-  (= __ (.toUpperCase "select * from"))
+  (= "SELECT * FROM" (to-upper-case "select * from"))
 
   "But instance method calls are very different from normal functions"
-  (= ["SELECT" "FROM" "WHERE"] (map ___ ["select" "from" "where"]))
+  (= ["SELECT" "FROM" "WHERE"] (map to-upper-case ["select" "from" "where"])) ;; this is why all java should stay out of my code, in separate function
 
   "Constructing might be harder than breaking"
-  (= 10 (let [latch (java.util.concurrent.CountDownLatch. __)]
-          (.getCount latch)))
+  (= 10 (let [latch (make-latch 10)] (get-count latch)))
 
   "Static methods are slashing prices!"
-  (== __ (Math/pow 2 10)))
+  (== 1024.0 (pow 2 10)))
