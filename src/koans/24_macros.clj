a138d0dc03e0b482529627ffa42fc1272dd83287
Dmitry Moskowski <me@corpix.ru>
2019-12-14 22:02:26 +0000

    koans: 24_macros

--- src/koans/24_macros.clj
+++ src/koans/24_macros.clj
@@ -5,35 +5,48 @@
   (str "Hello, " x))
 
 (defmacro infix [form]
-  (list (second form) (first form) (nth form 2)))
+  (list (second form)
+        (first  form)
+        (last   form)))
 
 (defmacro infix-concise [form]
-  `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
-    __
-    __))
+  `(~(second form)
+    ~(first  form)
+    ~(last   form)))
 
-(defmacro recursive-infix [form]
-  (cond (not (seq? form))
-        __
-        (= 1 (count form))
-        `(recursive-infix ~(first form))
-        :else
-        (let [operator (second form)
-              first-arg (first form)
-              others __]
-          `(~operator
-            (recursive-infix ~first-arg)
-            (recursive-infix ~others)))))
+(defmacro infix-recursive [form]
+  (cond
+    (not (seq? form)) form
+    (= (count form) 1) `(infix-recursive ~(first form))
+
+    ;; require triples to make a user use parens in expressions
+    ;; otherwise we need to implement operator precendense ;)
+    (not (= (count form) 3))
+    (throw (let [expr (seq form)]
+             (ex-info
+              (format "expression %s should be a list of 3 items" expr)
+              {:cause #{:even-arguments-count}
+               :expr  expr})))
+
+    :else (let [op (nth form 1)
+                x  (nth form 0)
+                xs (drop 2 form)]
+            `(~op
+              (infix-recursive ~x)
+              (infix-recursive ~xs)))))
 
 (meditations
   "Macros are like functions created at compile time"
-  (= __ (hello "Macros!"))
+  (= "Hello, Macros!" (hello "Macros!"))
 
   "I can haz infix?"
-  (= __ (infix (9 + 1)))
+  (= 10 (infix (9 + 1)))
+
+  "I can haz recursive infix?"
+  (= 20 (infix-recursive ((9 + 1) * 2)))
 
   "Remember, these are nothing but code transformations"
-  (= __ (macroexpand '(infix (9 + 1))))
+  (= '(+ 9 1) (macroexpand '(infix (9 + 1))))
 
   "You can do better than that - hand crafting FTW!"
   (= '(* 10 2) (macroexpand '(infix-concise (10 * 2))))
@@ -42,4 +55,4 @@
   (= '(+ 10 (2 * 3)) (macroexpand '(infix-concise (10 + (2 * 3)))))
 
   "Really, you don't understand recursion until you understand recursion"
-  (= 36 (recursive-infix (10 + (2 * 3) + (4 * 5)))))
+  (= 36 (infix-recursive (10 + ((2 * 3) + (4 * 5))))))
