a3653696150f7eb826fe8153b6dc86e9fa95522e
Dmitry Moskowski <me@corpix.ru>
2019-12-14 22:47:04 +0000

    koans: 25_threading_macros

--- src/koans/25_threading_macros.clj
+++ src/koans/25_threading_macros.clj
@@ -1,11 +1,10 @@
 (ns koans.25-threading-macros
   (:require [koan-engine.core :refer :all]))
 
-(def a-list
-  '(1 2 3 4 5))
+(def a-list '(1 2 3 4 5))
+(def a-list-with-maps '({:a 1} {:a 2} {:a 3}))
 
-(def a-list-with-maps
-  '({:a 1} {:a 2} {:a 3}))
+(def trim clojure.string/trim)
 
 (defn function-that-takes-a-map [map a b]
   (get map :a))
@@ -15,52 +14,49 @@
 
 (meditations
  "We can use thread first for more readable sequential operations"
- (= __
-    (-> {}
-        (assoc :a 1)))
+ (= {:a 1}
+    (-> {} (assoc :a 1)))
 
  "Consider also the case of strings"
- (= __
+ (= "Hello world, and moon, and stars"
     (-> "Hello world"
         (str ", and moon")
         (str ", and stars")))
 
  "When a function has no arguments to partially apply, just reference it"
- (= __
-    (-> "String with a trailing space "
-        clojure.string/trim))
+ (= "String with a trailing space"
+    (-> "String with a trailing space " trim))
 
  "Most operations that take a scalar value as an argument can be threaded-first"
- (= __
+ (= 6
     (-> {}
         (assoc :a 1)
         (assoc :b 2)
-        (assoc :c {:d 4
-                   :e 5})
+        (assoc :c {:d 4 :e 5})
         (update-in [:c :e] inc)
         (get-in [:c :e])))
 
  "We can use functions we have written ourselves that follow this pattern"
- (= __
+ (= 1
     (-> {}
         (assoc :a 1)
         (function-that-takes-a-map "hello" "there")))
 
  "We can also thread last using ->>"
- (= __
+ (= [2 3 4]
     (->> [1 2 3]
          (map inc)))
 
  "Most operations that take a collection can be threaded-last"
- (= __
+ (= 12
     (->> a-list
          (map inc)
          (filter even?)
          (into [])
          (reduce +)))
 
- "We can use funtions we have written ourselves that follow this pattern"
- (= __
+ "We can use functions we have written ourselves that follow this pattern"
+ (= [1 2 3]
     (->> a-list-with-maps
          (function-that-takes-a-coll "hello" "there")
          (into []))))
